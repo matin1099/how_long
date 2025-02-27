fcb1710810e81156723269e818d042a572ce1167
matin1099 <matin1378718@gmail.com>
2025-02-27 17:32:25 +0330

    i dont know what was it!!!

--- info.py
+++ info.py
@@ -1,39 +1,54 @@
-"""
+
 #  https://www.geeksforgeeks.org/get-video-duration-using-python-opencv/
-# import module 
 import cv2 
 import datetime 
 
-# create video capture object 
-data = cv2.VideoCapture('C:/Users/Asus/Documents/videoDuration.mp4') 
 
-# count the number of frames 
-frames = data.get(cv2.CAP_PROP_FRAME_COUNT) 
-fps = data.get(cv2.CAP_PROP_FPS) 
+class start():
+    def __init__(self, url):
+        self.url = url
 
-# calculate duration of the video 
-seconds = round(frames / fps) 
-video_time = datetime.timedelta(seconds=seconds) 
-print(f"duration in seconds: {seconds}") 
-print(f"video time: {video_time}") 
-"""
-def get_files(url:str)-> str:
-    """get path of folder--> return list of video files
+    def get_files(url:str)-> str:
+        """get path of folder--> return list of video files
 
-    Args:
-        url (str): path of folder with videos
+        Args:
+            url (str): path of folder with videos
 
-    Returns:
-        str: list contain FULL video path
-    """
-    pass 
-def swing(name:str)->str:
-    """swing full pass to video names
+        Returns:
+            str: list contain FULL video path
+        """
+        pass     
+# The previous 2 lines can also be written as
+# Person().say_hi()
 
-    Args:
-        name (str): video names
+    def swing(name:str)->str:
+        """swing full pass to video names
 
-    Returns:
-        str: full path name
-    """
-    pass
+        Args:
+            name (str): video names
+
+        Returns:
+            str: full path name
+        """
+        pass
+    def during(data)-> str:
+        """get path of folder--> return list of video files
+
+        Args:
+            url (str): path of folder with videos
+
+        Returns:
+            str: list contain FULL video path
+        """
+        # create video capture object 
+        data = cv2.VideoCapture('C:/Users/Asus/Documents/videoDuration.mp4') 
+
+        # count the number of frames 
+        frames = data.get(cv2.CAP_PROP_FRAME_COUNT) 
+        fps = data.get(cv2.CAP_PROP_FPS) 
+
+        # calculate duration of the video 
+        seconds = round(frames / fps) 
+        video_time = datetime.timedelta(seconds=seconds) 
+        print(f"duration in seconds: {seconds}") 
+        print(f"video time: {video_time}") 
--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-import info
-info.get_files()
+import os 
+def get_files(url:str)-> str:
+        """get path of folder--> return list of video files
+
+        Args:
+            url (str): path of folder with videos
+
+        Returns:
+            str: list contain FULL video path
+        """
+        print(os.listdir(url))
+
+get_files("/home/matin-uni/Downloads/Videos")
+# import module 
+import cv2 
+import datetime 
+
+# create video capture object 
+data = cv2.VideoCapture('/home/matin-uni/Downloads/Videos/Despicable Me 4.[SS][720][AioFilm.com].mkv') 
+
+# count the number of frames 
+frames = data.get(cv2.CAP_PROP_FRAME_COUNT) 
+fps = data.get(cv2.CAP_PROP_FPS) 
+
+# calculate duration of the video 
+seconds = round(frames / fps) 
+video_time = datetime.timedelta(seconds=seconds) 
+print(f"duration in seconds: {seconds}") 
+print(f"video time: {video_time}") 
