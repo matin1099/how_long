2435e0b0b3a3df4c9c0dbb865a494bbb24621e66
matin1099 <matin1378718@gmail.com>
2025-02-28 00:36:53 +0330

    fix for using sub folder

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import os
+import sys
 import cv2 
 import datetime 
+
 from loguru import logger
 
-def filefinder():
+
+def filefinder(subfolder:str):
+    if subfolder != './':
+        Issub = True 
+    else:
+        Issub = False
+
     abs_address = os.getcwd()
     logger.info('Getting full address.')
-    dir_files = os.listdir()
+    dir_files = os.listdir(subfolder)
     logger.info('Getting folder files.')
     vids=[]
     
     logger.debug('Start to search for mp4s.')
     for file in dir_files:
-        if file[-3:] == 'mp4':
+        if file[-3:] == 'mp4' and Issub == True:
+            vids.append(abs_address+'/'+subfolder+file)
+        elif file[-3:] == 'mp4' and Issub == True:
             vids.append(abs_address+'/'+file)
 
     return(vids)
@@ -39,5 +49,10 @@ def counter(video_path:list):
     print(f'Whole folder will be {folder_time}')
 
 if __name__=='__main__':
-    vid_list = filefinder()
+    term_args = sys.argv[:]
+    try:
+        subfolder = term_args[1]+'/'
+    except IndexError:
+        subfolder='./'
+    vid_list = filefinder(subfolder)
     counter(video_path=vid_list)
