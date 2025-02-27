54951f911046cda6be495d3f85fa2ebc320ccc3e
matin1099 <matin1378718@gmail.com>
2025-02-27 23:44:06 +0330

    one fileing done.
    add feature like going deep.

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-import os 
-def get_files(url:str)-> str:
-        """get path of folder--> return list of video files
-
-        Args:
-            url (str): path of folder with videos
-
-        Returns:
-            str: list contain FULL video path
-        """
-        print(os.listdir(url))
-
-get_files("/home/matin-uni/Downloads/Videos")
-# import module 
+import os
 import cv2 
 import datetime 
+from loguru import logger
 
-# create video capture object 
-data = cv2.VideoCapture('/home/matin-uni/Downloads/Videos/Despicable Me 4.[SS][720][AioFilm.com].mkv') 
+def filefinder():
+    abs_address = os.getcwd()
+    logger.info('Getting full address.')
+    dir_files = os.listdir()
+    logger.info('Getting folder files.')
+    vids=[]
+    
+    logger.debug('Start to search for mp4s.')
+    for file in dir_files:
+        if file[-3:] == 'mp4':
+            vids.append(abs_address+'/'+file)
 
-# count the number of frames 
-frames = data.get(cv2.CAP_PROP_FRAME_COUNT) 
-fps = data.get(cv2.CAP_PROP_FPS) 
+    return(vids)
 
-# calculate duration of the video 
-seconds = round(frames / fps) 
-video_time = datetime.timedelta(seconds=seconds) 
-print(f"duration in seconds: {seconds}") 
-print(f"video time: {video_time}") 
+def duration(pathtofile:str):
+    data = cv2.VideoCapture(pathtofile)
+    frames = data.get(cv2.CAP_PROP_FRAME_COUNT) 
+    fps = data.get(cv2.CAP_PROP_FPS)
+    logger.trace(f'is open:\t{data.isOpened()}') 
+    logger.trace(f'path:\t{pathtofile}') 
+    logger.trace(f'fps:\t{fps}') 
+    logger.trace(f'frames:\t{frames}') 
+
+    seconds = round(frames / fps) 
+    return seconds
+
+
+def counter(video_path:list):
+    fulltime = 0
+    for path in video_path:
+        fulltime += duration(path)
+
+    folder_time = datetime.timedelta(seconds=fulltime) 
+    print(f'Whole folder will be {folder_time}')
+
+if __name__=='__main__':
+    vid_list = filefinder()
+    counter(video_path=vid_list)
